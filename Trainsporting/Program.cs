03cab332c198eb292401d7548ee15973b17127bd
kosma1996 <kosma1996@gmail.com>
2018-01-31 02:14:46 +0100

    Initial commit

---
+++ Trainsporting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+namespace Trainsporting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game())
+            {
+
+                game.Run(30,30);
+
+            }
+        }
+    }
+}
